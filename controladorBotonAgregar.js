545dd9445b1c01f67d359973e4631171e09beb4f
AlanRcq <acueto@fi.uba.ar>
2022-03-18 01:07:10 -0300

    Agrego la funcionalidad del popup, cambio la estética de la aplicació, y ya no se puede agregar una ubicación a menos que todos los datos hayan sido completados.

--- controladorBotonAgregar.js
+++ controladorBotonAgregar.js
@@ -6,25 +6,52 @@ let inputCoordenadaY = document.getElementById('coordenadaY')
 let inputCategoria = document.getElementById('categoria')
 
 
+
+
 $('#botonAgregar').click(function(){
-    console.log(coordenadaX.value)
-    if (!coordenadasValidas(coordenadaX.value, coordenadaY.value)){
-        alert("Las coordenadas que ingresaste no son válidas. \nRecuerda: \nvalores posibles Longitud -180 < x < 180 ; Latitud -90 < y <  90 ");
+    if (!datosCompletados()){
+        alert("Hay datos sin completar");
+    } else if (!coordenadasValidas(coordenadaX.value, coordenadaY.value)){
+        alert("Las coordenadas que ingresaste no son válidas. \nRecuerda que la longitud x debe ser tal que -180 < x < 180 y la latitud y debe ser tal que -90 < y < 90");
         return;
     } else {
-        let elemento = document.createElement('div');
-        elemento.className = 'marker';
-        let marker = new mapboxgl.Marker(elemento)
-        .setLngLat({
-        lng: inputCoordenadaX.value,
-        lat: inputCoordenadaY.value,
-        })
-        .addTo(map);
+        agregarUbicacion();
     }
 })
 
-function coordenadasValidas(coordenadaX, coordenadaY){
-    let coordenadaXValida = (-180 < coordenadaX) && (coordenadaX < 180);
-    let coordenadaYValida = (-90 < coordenadaY) && (coordenadaY < 90);
+function agregarUbicacion(){
+    let elemento = document.createElement('div');
+    elemento.className = 'marker';
+    let marker = new mapboxgl.Marker(elemento)
+    let markerPopup = new mapboxgl.Popup();
+    markerPopup.setHTML(htmlPopup());
+    marker.setPopup(markerPopup);
+    marker.setLngLat({
+        lng: inputCoordenadaX.value,
+        lat: inputCoordenadaY.value,
+    })
+    marker.addTo(map);
+    }
+
+function coordenadasValidas(){
+    let coordenadaXValida = (-180 < inputCoordenadaX.value) && (inputCoordenadaX.value < 180);
+    let coordenadaYValida = (-90 < inputCoordenadaY.value) && (inputCoordenadaY.value < 90);
     return coordenadaXValida && coordenadaYValida;
+}
+
+function datosCompletados(){
+    let nombreCompletado = inputNombre.value != "";
+    let direccionCompletada = inputDireccion.value != "";
+    let telefonoCompletado = inputNumeroTel.value != "";
+    let categoriaCompletada = inputCategoria.value != "Seleccionar";
+    return (nombreCompletado && direccionCompletada && telefonoCompletado && categoriaCompletada);
+}
+
+function htmlPopup(){
+    let labelNombre = '<b>Nombre: </b><label>' + inputNombre.value + '</label><br>';
+    let labelDireccion = '<b>Direccion: </b><label>' + inputDireccion.value + '</label><br>';
+    let labelTelefono= '<b>Teléfono: </b><label>' + inputNumeroTel.value + '</label><br>';
+    let labelCoordenadas = '<b>(X, Y): </b><label>' + inputCoordenadaX.value + ", " + inputCoordenadaY.value+ '</label><br>';
+    let labelCategoria = '<b>Categoría: </b><label>' + inputCategoria.value + '</label>';
+    return labelNombre + labelDireccion + labelTelefono + labelCoordenadas + labelCategoria;
 }
